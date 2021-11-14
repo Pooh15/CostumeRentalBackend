1d9fd9ac4c693fad9347b7c023dfd5bc45a518ae
sasneprasanna <psasne@oakland.edu>
2021-11-13 22:14:53 -0500

    addItem in inventory

--- routes/category.js
+++ routes/category.js
@@ -1,54 +1,5 @@
 let express = require('express');
 let router = express.Router();
-let multer = require('multer');
-
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage, limits: {
-	fieldSize: 1 * 1024 * 1024
-}
-});
-
-router.post('/abc', function(req, res){
-	let output={};
-	console.log(req.body);
-
-	if(req.body.fname != '' && req.body.lname != ''){
-
-		req.getConnection((error, conn) =>{
-			upload.single('imageName')(req, res, function(err) {
-				if (err) {
-		 // An error occurred when uploading
-		 console.log('Err: ', err);
-		 return;
-		} else {
-			let imgName = Object.values(req.files)[0].name.split('.');
-			imgName = imgName[0].split(' ').join('')+'-'+Date.now();
-			let imageBlob = Object.values(req.files)[0].data.toString('base64');
-
-			console.log(imgName);
-			console.log(Object.values(req.files)[0])
-			let query = `INSERT INTO image SET ?`;
-			conn.query(query, {image_name: imgName, image: imageBlob}, (err, results, fields) => {
-				if (err) {
-					return console.error(err.message);
-				}
-				console.log('Row inserted:' + results.affectedRows);
-				output["status"]=1;
-				output["message"]="Image added Successfully!";
-				res.send(output);
-			});
-			
-			
-
-		} })
-		});
-	} else {
-		output["status"]=0;
-		output["message"]="Please enter All fields";
-		res.send(output);
-	}
-
-});
 
 router.post('/create', function(req, res){
 	let query = `select * from category where c_name = ?`;
--- routes/inventory.js
+++ routes/inventory.js
@@ -1,7 +1,56 @@
 let express = require('express');
-let app = express();
+let router = express.Router();
+let multer = require('multer');
 
-app.get('/getDetails', (req, res) =>{
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage, limits: {
+	fieldSize: 1 * 1024 * 1024
+}
+});
+
+router.post('/addItem', function(req, res){
+	let output={};
+	console.log(req.body);
+
+	if(req.body.fname != '' && req.body.lname != ''){
+
+		req.getConnection((error, conn) =>{
+			upload.single('imageName')(req, res, function(err) {
+				if (err) {
+		 // An error occurred when uploading
+		 console.log('Err: ', err);
+		 return;
+		} else {
+			let imgName = Object.values(req.files)[0].name.split('.');
+			imgName = imgName[0].split(' ').join('')+'-'+Date.now();
+			let imageBlob = Object.values(req.files)[0].data.toString('base64');
+
+			console.log(imgName);
+			console.log(Object.values(req.files)[0])
+			let query = `INSERT INTO image SET ?`;
+			conn.query(query, {image_name: imgName, image: imageBlob}, (err, results, fields) => {
+				if (err) {
+					return console.error(err.message);
+				}
+				console.log('Row inserted:' + results.affectedRows);
+				output["status"]=1;
+				output["message"]="Image added Successfully!";
+				res.send(output);
+			});
+			
+
+
+		} })
+		});
+	} else {
+		output["status"]=0;
+		output["message"]="Please enter All fields";
+		res.send(output);
+	}
+
+});
+
+router.get('/getDetails', (req, res) =>{
 	req.getConnection((error, conn) =>{
 		let output={};
 
@@ -21,4 +70,4 @@ app.get('/getDetails', (req, res) =>{
 	});
 });
 
-module.exports = app;	
+module.exports = router;	
