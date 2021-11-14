28c96dfc0edabaa8d9b290399a52cc66eea05fbc
sasneprasanna <psasne@oakland.edu>
2021-11-13 21:37:34 -0500

    upload image success

--- routes/category.js
+++ routes/category.js
@@ -4,30 +4,50 @@ let multer = require('multer');
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage, limits: {
-	fieldSize: 10 * 1024 * 1024
+	fieldSize: 1 * 1024 * 1024
 }
 });
 
-//router.post('/abc', upload.single('imageName'), function(req, res){
-router.route("/abc").post(upload.single("imageName"), (req, res) => {	
-	//console.log(Object.values(req.files)[0].data.toString('base64'));
-	console.log(Object.values(req.files)[0].name);
+router.post('/abc', function(req, res){
+	let output={};
 	console.log(req.body);
-	/*upload.single('imageName')(req, res, function(err) {
-      if (err) {
-         // An error occurred when uploading
-         console.log('Err: ', err);
-         return;
-      } else {
-        // console.log('req.file: ', JSON.stringify(req.file));
-       // req.file.buffer.toString('base64')
-         console.log('req.files: ', req.files.);
-         return;
-      }
-  })*/
-  res.send({
-  	'hi': 'bye'
-  })
+
+	if(req.body.fname != '' && req.body.lname != ''){
+
+		req.getConnection((error, conn) =>{
+			upload.single('imageName')(req, res, function(err) {
+				if (err) {
+		 // An error occurred when uploading
+		 console.log('Err: ', err);
+		 return;
+		} else {
+			let imgName = Object.values(req.files)[0].name.split('.');
+			imgName = imgName[0].split(' ').join('')+'-'+Date.now();
+			let imageBlob = Object.values(req.files)[0].data.toString('base64');
+
+			console.log(imgName);
+			console.log(Object.values(req.files)[0])
+			let query = `INSERT INTO image SET ?`;
+			conn.query(query, {image_name: imgName, image: imageBlob}, (err, results, fields) => {
+				if (err) {
+					return console.error(err.message);
+				}
+				console.log('Row inserted:' + results.affectedRows);
+				output["status"]=1;
+				output["message"]="Image added Successfully!";
+				res.send(output);
+			});
+			
+			
+
+		} })
+		});
+	} else {
+		output["status"]=0;
+		output["message"]="Please enter All fields";
+		res.send(output);
+	}
+
 });
 
 router.post('/create', function(req, res){
